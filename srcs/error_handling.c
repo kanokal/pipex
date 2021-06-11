b5cc196d50a11e1901e1734329260bec3f476aa7
jpyo <jpyo@student.42.fr>
2021-06-11 18:44:51 +0900

    Create error handling

---
+++ srcs/error_handling.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handling.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/11 18:27:43 by jpyo              #+#    #+#             */
+/*   Updated: 2021/06/11 18:31:02 by jpyo             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/pipex.h"
+
+void	pipex_malloc_error(char *s1, char *s2, char **split1, char **split2)
+{
+	if (s1 != NULL)
+		free(s1);
+	if (s2 != NULL)
+		free(s2);
+	if (split1 != NULL)
+		ft_free_split(split1);
+	if (split2 != NULL)
+		ft_free_split(split2);
+	write(2, "error: malloc failed\n", 21);
+	exit(-2);
+}
+
+void	pipex_open_error(int err)
+{
+	perror("open");
+	exit(err);
+}
