d000fe5f8e374017ec173ad745d7ff9154c0c485
JunSik Pyo <57995337+kanokal@users.noreply.github.com>
2021-06-12 17:50:06 +0900

    Update pipex_parent.c

--- srcs/pipex_parent.c
+++ srcs/pipex_parent.c
@@ -12,9 +12,45 @@
 
 #include "../includes/pipex.h"
 
+/*
+** wait 함수 리턴값 확인
+** fork 했을때 처음부터인지 아닌지 체크
+*/
+
+static void	pipex_parent_err(char **path, char**cmd, int out)
+{
+	ft_free_split(path);
+	ft_free_split(cmd);
+	close(out);
+	perror("execve");
+	exit(-1);
+}
+
 void	pipex_parent(int fd[2], int argc, char **argv, char **envp)
 {
+	int		idx;
+	int		out;
+	char	**path;
+	char	**cmd;
+	char	*execve_path;
+
 	wait(0);
+	if (dup2(fd[0], 0) < 0)
+		pipex_dup2_error();
 	close(fd[0]);
 	close(fd[1]);
+	out = open("outfile", O_WRONLY | O_CREAT | O_TRUNC);
+	if (out < 0)
+		pipex_open_error();
+	if (dup2(out, 1) < 0)
+		pipex_dup2_error();
+	pipex_get_path_n_cmd(&path, &cmd, argv, envp);
+	idx = 0;
+	while (path[idx] != 0)
+	{
+		execve_path = pipex_set_path(idx++, path, cmd);
+		execve(execve_path, cmd, envp);
+		ft_free_ptr(&execve_path);
+	}
+	return (pipex_parent_err(path, cmd, out));
 }
