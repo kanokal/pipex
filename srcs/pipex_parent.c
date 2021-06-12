354e97cf6fde3dcdd4a8a5f84972f2d3cc894c6e
jpyo <jpyo@student.42.fr>
2021-06-12 22:12:32 +0900

    Update with bonus

--- srcs/pipex_parent.c
+++ srcs/pipex_parent.c
@@ -6,51 +6,55 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 15:18:44 by jpyo              #+#    #+#             */
-/*   Updated: 2021/06/11 17:30:42 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/06/12 22:11:10 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-/*
-** wait 함수 리턴값 확인
-** fork 했을때 처음부터인지 아닌지 체크
-*/
-
-static void	pipex_parent_err(char **path, char**cmd, int out)
+static void	pipex_parent_err(char **path, char**cmd)
 {
 	ft_free_split(path);
 	ft_free_split(cmd);
-	close(out);
-	perror("execve");
-	exit(-1);
+	write(2, "execve: invalid command\n", 24);
+	exit(errno);
 }
 
-void	pipex_parent(int fd[2], int argc, char **argv, char **envp)
+static void	pipex_parent_do(t_pipex var)
 {
 	int		idx;
-	int		out;
 	char	**path;
 	char	**cmd;
 	char	*execve_path;
-
-	wait(0);
-	if (dup2(fd[0], 0) < 0)
-		pipex_dup2_error();
-	close(fd[0]);
-	close(fd[1]);
-	out = open("outfile", O_WRONLY | O_CREAT | O_TRUNC);
-	if (out < 0)
-		pipex_open_error();
-	if (dup2(out, 1) < 0)
-		pipex_dup2_error();
-	pipex_get_path_n_cmd(&path, &cmd, argv, envp);
+	
+	pipex_get_path_n_cmd(&path, &cmd, var);
 	idx = 0;
 	while (path[idx] != 0)
 	{
 		execve_path = pipex_set_path(idx++, path, cmd);
-		execve(execve_path, cmd, envp);
+		execve(execve_path, cmd, var.envp);
 		ft_free_ptr(&execve_path);
 	}
-	return (pipex_parent_err(path, cmd, out));
+	return (pipex_parent_err(path, cmd));
+}
+
+void	pipex_parent(t_pipex var)
+{
+	int		outfile;
+
+	if (dup2(var.fd[0], 0) < 0)
+		pipex_dup2_error(var, 1);
+	close(var.fd[0]);
+	close(var.fd[1]);
+	outfile = open(var.argv[var.last], O_WRONLY | O_CREAT | O_TRUNC,
+					S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (outfile < 0)
+		pipex_open_error();
+	if (dup2(outfile, 1) < 0)
+	{
+		close(outfile);
+		pipex_dup2_error(var, 0);
+	}
+	close(outfile);
+	return (pipex_parent_do(var));
 }
