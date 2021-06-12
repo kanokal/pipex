22604d1cfd3c3e181f6752039095e93fe3110b10
jpyo <jpyo@student.42.fr>
2021-06-12 22:11:47 +0900

    Fix function name

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/11 15:10:21 by jpyo              #+#    #+#             */
-/*   Updated: 2021/06/11 18:39:17 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/06/12 22:11:36 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,19 +17,19 @@
 # include <stdlib.h>
 # include <fcntl.h>
 # include <errno.h>
+# include "pipex_structure.h"
 # include "../libft/includes/libft.h"
 
-void	pipex_parent(int fd[2], int argc, char **argv, char **envp);
-void	pipex_child(int fd[2], int argc, char **argv, char **envp);
+void	pipex_one(t_pipex var);
+void	pipex_parent(t_pipex var);
+void	pipex_child(t_pipex var);
 
 char	**pipex_get_path(char **envp);
-char	*pipex_set_path(int i, char **absolute_path, char **cmd);
-void	pipex_get_path_n_cmd(char ***path, char ***cmd, char **argv, char **envp);
+char	*pipex_set_path(int i, char **path, char **cmd);
+void	pipex_get_path_n_cmd(char ***path, char ***cmd, t_pipex var);
 
 void	pipex_open_error(void);
-void	pipex_dup2_error(void);
+void	pipex_dup2_error(t_pipex var, int option);
 void	pipex_malloc_error(char *s1, char *s2, char **split1, char **split2);
-
-# include <stdio.h>
 
 #endif
