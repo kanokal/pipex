46aff5b6f3115bf2fa8e0e01c5f35d866760ca45
jpyo <jpyo@student.42.fr>
2021-06-24 20:24:34 +0900

    Chagne var name

--- includes/pipex_structure.h
+++ includes/pipex_structure.h
@@ -6,7 +6,7 @@
 /*   By: jpyo <jpyo@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/12 21:12:18 by jpyo              #+#    #+#             */
-/*   Updated: 2021/06/12 22:11:22 by jpyo             ###   ########.fr       */
+/*   Updated: 2021/06/24 20:07:27 by jpyo             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ typedef struct	s_pipex
 {
 	int			fd[2];
 	int			cur;
-	int			last;
+	int			cmd_count;
 	char		**argv;
 	char		**envp;
 }				t_pipex;
